1cafaf17b860499d1c4c308fdd69e441881ee411
a1l1ke <onetenthplease@gmail.com>
2026-06-25 11:43:05 +0900

    refactor: 인터페이스 이름 변경(NewsProvider/NewsPublisher) 및 NewsService 클래스 추가

--- src/oop/search/application/NewsProvider.java
+++ src/oop/search/application/NewsProvider.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * 뉴스 검색을 위한 인터페이스
  */
-public interface NewProvider {
+public interface NewsProvider {
     /// searchQuery : 검색, limit : 갯수 제한
     List<NewsResult> fetchNews(String searchQuery, int limit);
 }
